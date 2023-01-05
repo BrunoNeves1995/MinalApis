9e9d494763681bf071a56d49921ed236d9950871
Bruno Neves <nevesbruno814@gmail.com>
2023-01-05 10:21:06 -0400

    Metodos de mapeamento de rota

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/ {nome}", (string nome) => Results.Ok($"Olá {nome}"));
+
+app.MapPost("/", (User user) => Results.Ok($"Olá {user}"));
 
 app.Run();
+
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+}
